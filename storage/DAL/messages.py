1dff0585639dd1ea4a33f9240df794f0a5e76a5f
fthh <sample.fthh@gmail.com>
2019-08-17 22:29:22 +0700

    Add inserting user's messages

--- storage/DAL/messages.py
+++ storage/DAL/messages.py
@@ -2,6 +2,7 @@ import asyncpg
 
 
 async def insert_new_message(conn: asyncpg.connect, user: str, message: str):
-    # TODO
-    values = await conn.fetch('''SELECT * FROM messages''')
-    print(values)
+    await conn.fetch('''
+        INSERT INTO messages (user_name, message_text) 
+        VALUES ($1, $2)''',
+        user, message)
